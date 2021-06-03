d140245f2f57baee26621e3ea9cf9f814e53875b
karthik-8369 <84717577+karthik-8369@users.noreply.github.com>
2021-06-03 21:14:49 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -41,11 +41,13 @@ int main(void) {
 float mc_pi(int a)
 { 
   float count=0;
-  for(int i=0;i<a;i++){
+  for(int i=0;i<a;i++)
+  {
     float x=frandom();
     float y=frandom();
     float d=sqrt(pow(x,2)+pow(y,2));
-    if(d<1){
+    if(d<1)
+    {
      count++;
     }
   }
